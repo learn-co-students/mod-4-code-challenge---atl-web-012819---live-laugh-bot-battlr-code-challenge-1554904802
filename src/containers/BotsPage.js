116399625d5d665a364375c84c06e00d913c3cc6
wulman16 <will.ulman@gmail.com>
2019-04-10 10:50:47 -0400

    checkpoint done.

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,53 @@
 import React from "react";
+import YourBotArmy from "./YourBotArmy"
+import BotCollection from "./BotCollection"
+
+const API = `https://bot-battler-api.herokuapp.com/api/v1/bots`
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  state = {
+    bots: [],
+    selectedBots: []
+  }
+
+  handleData = (data) => {
+    this.setState({
+      bots: data
+    })
+  }
+
+  handleClick = (botId) => {
+    if (this.findSelectedBotById(botId)) {
+      this.setState({
+        selectedBots: this.state.selectedBots.filter(bot => bot.id !== botId)
+      })
+    } else {
+      this.setState({
+        selectedBots: this.state.selectedBots.concat(this.findBotById(botId))
+      })
+    }
+  }
+
+  findBotById = (botId) => {
+    return this.state.bots.find(bot => bot.id === botId)
+  }
+
+  findSelectedBotById = (botId) => {
+    return this.state.selectedBots.find(bot => bot.id === botId)
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => this.handleData(data))
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy selectedBots={this.state.selectedBots} handleClick={this.handleClick}/>
+        <BotCollection bots={this.state.bots} handleClick = {this.handleClick}/>
       </div>
     );
   }
