7f040fb6dc04c79a895532e0ed51196378ca7ef9
Leigh Baugham <leigh.baugham@gmail.com>
2019-04-10 10:53:10 -0400

    enlist army works

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,48 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+
+
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
 
-  render() {
-    return (
-      <div>
-        {/* put your components here */}
-      </div>
-    );
-  }
+    state = {
+        bots: []
+    }
 
+    componentDidMount() {
+        fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+        .then(res => res.json())
+        .then(data => {
+            const botsAddEnlist = data.map(bot => ({ ...bot, enlisted: false}))
+            this.setState({ bots: botsAddEnlist })
+        })
+    }
+
+    enlistBot = (bot) => {
+        const enlistedBots = this.state.bots.map((botElement) => (bot.id === botElement.id ? ({...botElement, enlisted: !botElement.enlisted}) : botElement ))
+        this.setState({ bots: enlistedBots})
+    }
+
+    render() {
+
+        let enlistArray = []
+        let botsArray = []
+
+        this.state.bots.map((bot) => {
+            if(bot.enlisted)
+            enlistArray.push(bot)
+            else 
+            botsArray.push(bot)
+        })
+        return (
+            <div>
+                <YourBotArmy bots={enlistArray} enlistBot={this.enlistBot}/>
+                
+                <BotCollection bots={botsArray} enlistBot={this.enlistBot}/>
+            </div>
+        );
+    }
 }
 
 export default BotsPage;
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -2,15 +2,17 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
-  //your bot army code here...
+  currentArmy = () => {
+    return this.props.bots.map((bot) => (bot.enlisted ? <BotCard bot={bot} handleClick={this.props.enlistBot} /> : null ))
+}
 
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            {this.currentArmy()}
+            
           </div>
         </div>
       </div>
